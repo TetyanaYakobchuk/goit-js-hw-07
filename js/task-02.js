e4612a9bf25fd8c7d210ac3cf003a0b424a315ea
Tetyana Yakobchuk <tetyana.yakobchuk@gmail.com>
2021-04-17 15:45:06 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsCollection = document.querySelector('ul');
+console.log(ingredientsCollection);
+
+const elements = ingredients.map(ingredient => {
+    const element = document.createElement('li');
+    element.append(...ingredient);
+    return element.textContent+' ';
+});
+console.log(elements);
+
+ingredientsCollection.append(...elements);
