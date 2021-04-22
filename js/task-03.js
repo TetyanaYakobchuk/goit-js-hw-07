e6c5f4c55dfdbdbe233a7d118140460d27c9dde4
Tetyana Yakobchuk <tetyana.yakobchuk@gmail.com>
2021-04-22 16:44:28 +0300

    Update task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,27 @@
+
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const listGallery = document.getElementById("gallery");
+images.forEach((elem, index) => {
+  const itemGallery = `<li><img src="${elem.url}" alt="${elem.alt}" width="480" height="280"></li>`;
+  listGallery.insertAdjacentHTML("beforeend", itemGallery);
+  const gallery_item = listGallery.children[index];
+  gallery_item.classList.add("item-gallery");
+  console.log(gallery_item);
+});
